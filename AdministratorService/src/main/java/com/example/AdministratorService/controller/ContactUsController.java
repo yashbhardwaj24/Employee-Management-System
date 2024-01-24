c51fcd09e70b4cb042e6404c3d0e5fdd8f15d301
yashbhardwaj24 <94592850+yashbhardwaj24@users.noreply.github.com>
2024-01-24 17:32:26 +0530

    Add files via upload

---
+++ AdministratorService/src/main/java/com/example/AdministratorService/controller/ContactUsController.java
@@ -0,0 +1,59 @@
+package com.example.AdministratorService.controller;
+
+import com.example.AdministratorService.model.ContactUsModel;
+import com.example.AdministratorService.repository.ContactUsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/contactUs")
+public class ContactUsController {
+    @Autowired
+    ContactUsRepository contactUsRepository;
+
+
+    @GetMapping("/test")
+    public String test(){
+        return "working";
+    }
+
+    @GetMapping("/readAllQuery")
+    public List<ContactUsModel> readAllQuery(){
+        return contactUsRepository.findAll();
+    }
+
+    @PostMapping("/createQuery")
+    public Map<String, String> createQuery(
+           @RequestParam("name") String name,
+           @RequestParam("email") String email,
+           @RequestParam("query") String query
+    ) {
+
+        if (contactUsRepository.existsByEmail(email)){
+            return new HashMap<>(){{
+                put("status", "fail");
+                put("message", "Query with this email exists please wait for it to be resolved");
+            }};
+        }
+
+
+        ContactUsModel contactUsModel = ContactUsModel.builder()
+                .name(name)
+                .email(email)
+                .query(query)
+                .solved(false)
+                .build();
+
+        contactUsModel = contactUsRepository.save(contactUsModel);
+
+        return new HashMap<>() {{
+            put("status", "success");
+            put("message", "Thanks for contacting us " + name + " we will respond you at " + email);
+        }};
+    }
+}
